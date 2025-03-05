a8d2df595459786c38b2afac8e4f608e39744b69
Fabio Pelizzola <fbp@bendingspoons.com>
2025-03-05 12:10:36 +0100

    chore: add broken example

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -5,114 +5,135 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import type {ReactNode} from 'react';
 import {
+  Button,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Animated,
+  Easing,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [showToast, setShowToast] = useState<boolean>(false);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={{ height: '100%', width: '100%', backgroundColor: 'green' }}>
+      <AnimateToast showToast={showToast} onHideToast={() => setShowToast(false)} />
+      <View>
+        <View style={{ position: 'absolute', top: 0, height: 400, width: 200, backgroundColor: 'red' }} />
+        <Button title="Click me" onPress={() => setShowToast(true)}/>
+      </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
+
+
+export interface ToastPresenterProps {
+  toast: IToastChildren | null;
+  children: React.ReactNode;
+  onHideToast: () => void;
+}
+
+export type ToastData = string | ReactNode;
+export enum ToastType {
+  Info,
+  Error,
+}
+export interface IToastChildren {
+  data: ToastData;
+  displayTime?: number;
+  toastType: ToastType;
+}
+
+export function AnimateToast({
+  showToast,
+  onHideToast,
+}: {
+  showToast: boolean;
+  onHideToast: () => void;
+}): JSX.Element | null {
+  const [shouldRender, setShouldRender] = useState(false);
+  const animatedPosition = useMemo(() => new Animated.Value(-75), []);
+  const animatedOpacity = useMemo(() => new Animated.Value(0), []);
+
+  const animate = useCallback(
+    (show: boolean) => {
+      animatedOpacity.stopAnimation();
+      animatedPosition.stopAnimation();
+      
+      if (show) {
+        setShouldRender(true);
+      }
+
+      Animated.parallel([
+        Animated.timing(animatedOpacity, {
+          toValue: show ? 1 : 0,
+          duration: 300,
+          easing: Easing.inOut(Easing.ease),
+          useNativeDriver: true,
+        }),
+        Animated.timing(animatedPosition, {
+          toValue: show ? 0 : -75,
+          duration: 300,
+          easing: Easing.inOut(Easing.ease),
+          useNativeDriver: true,
+        }),
+      ]).start(({ finished }) => {
+        if (finished && !show) {
+          setShouldRender(false);
+        }
+      });
+    },
+    [animatedOpacity, animatedPosition]
+  );
+
+  useEffect(() => {
+    if (showToast) {
+      animate(true);
+      
+      const timer = setTimeout(() => {
+        animate(false);
+        onHideToast();
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    } else {
+      animate(false);
+    }
+  }, [showToast, animate, onHideToast]);
+
+  if (!shouldRender && !showToast) {
+    return null;
+  }
+
+  return (
+    <Animated.View
+      style={[
+        // eslint-disable-next-line react-native/no-inline-styles
+        {
+          zIndex: 1,
+          position: 'absolute',
+          alignSelf: 'center',
+          transform: [{ translateY: animatedPosition }],
+          opacity: animatedOpacity,
+          top: 100,
+        },
+      ]}
+    >
+      <Toast />
+    </Animated.View>
+  );
+}
+
+export const Toast = (): JSX.Element => {
+  return (
+    <View>
+      <Text>Toast</Text>
+    </View>
+  );
+};
